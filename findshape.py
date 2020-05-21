e9bd1393042156e492ca9bfb1c39d6ff1408b3f4
ismarintan98 <ismarintan98@gmail.com>
2020-05-22 01:20:35 +0700

    change contour color

--- findshape.py
+++ findshape.py
@@ -5,19 +5,19 @@ font = cv2.FONT_HERSHEY_COMPLEX
 
 cap = cv2.VideoCapture()
 
-rect_x_max = np.full(50, 0)
-rect_y_max = np.full(50, 0)
+rect_x_max = np.full(1000, 0)
+rect_y_max = np.full(1000, 0)
 
-rect_x_min = np.full(50, 0)
-rect_y_min = np.full(50, 0)  
+rect_x_min = np.full(1000, 0)
+rect_y_min = np.full(1000, 0)  
 
 number_of_rect = 0
 detetected = 0
 
 # img = cv2.imread("test5.png", cv2.IMREAD_GRAYSCALE)
-img = cv2.imread("test5.png")
+img = cv2.imread("testbw.png")
 img_grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-_, threshold = cv2.threshold(img_grey, 70, 255, cv2.THRESH_BINARY)
+_, threshold = cv2.threshold(img_grey, 88, 255, cv2.THRESH_BINARY)
 contours, _ = cv2.findContours(
     threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -80,7 +80,7 @@ for cnt in contours:
         for cnt_trig in range(0, number_of_rect):
             if x > rect_x_min[cnt_trig] and x < rect_x_max[cnt_trig] and y > rect_y_min[cnt_trig] and y < rect_y_max[cnt_trig]:
                 cv2.putText(img, "Triangle", (x, y), font, 1, (0))
-                cv2.drawContours(img, [approx], 0, (0), 3)
+                cv2.drawContours(img, [approx], 0, (21,21,255), 3)
                 
 
 cv2.imshow("shapes", img)
